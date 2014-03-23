35f81b76235c6cb112c44016d240b488568ae797
meselgroth <meselgroth@yahoo.com>
2014-03-23 16:29:27 +0000

    Inital commit. Need to use bower to bring down angularjs.

---
+++ app.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var superBetter = angular.module('superBetter', ['ngRoute']);
+
+	superBetter.config(function ($routeProvider) {
+		$routeProvider
+			.when('/',
+				{
+					controller: 'EmotionalController',
+					templateUrl: 'views/emotional.html'
+				})
+		.otherwise({ redirectTo: '/' });
+	});
+
+
+	//superBetter.controller('EmotionalController', function EmotionalController($scope) {
+	//	$scope.photo = 'https://onedrive.live.com/#cid=F0208DBD8F96ED0A&id=F0208DBD8F96ED0A%216180&v=3';
+	//});
+})();
---
+++ controllers/EmotionalController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'EmotionalController';
+
+    angular.module('superBetter').controller(controllerId,
+        ['$scope', EmotionalController]);
+
+	function EmotionalController($scope) {
+		$scope.photo = 'https://onedrive.live.com/#cid=F0208DBD8F96ED0A&id=F0208DBD8F96ED0A%216180&v=3';
+	}
+})();
