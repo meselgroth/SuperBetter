89055ffc1edfa9fa145c71446d6a1832f7049746
meselgroth <michael.eselgroth@hotels4u.com>
2014-08-19 10:01:27 +0100

    Add bootstrap. Add solution file to ease opening in VS.

--- controllers/EmotionalController.js
+++ controllers/EmotionalController.js
@@ -4,9 +4,13 @@
     var controllerId = 'EmotionalController';
 
     angular.module('superBetter').controller(controllerId,
-        ['$scope', EmotionalController]);
+        ['$scope', '$http', EmotionalController]);
 
-	function EmotionalController($scope) {
-	    $scope.photo = 'https://onedrive.live.com/embed?cid=F0208DBD8F96ED0A&resid=F0208DBD8F96ED0A%216180&authkey=ALpOxad6S8dLocA';
+    function EmotionalController($scope, $http) {
+        $http.get('resilience.json')
+       .then(function (data) {
+           $scope.photo = data.emotional.photo;
+       });
+	    //$scope.photo = 'https://scontent-b-lhr.xx.fbcdn.net/hphotos-prn2/t1.0-9/1781917_10152255627643188_421273889_n.jpg';
 	}
 })();
